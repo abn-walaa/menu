3f46f75bab8a6d07b74ee7a674ac3e1cab7e750e
Mohammed Walaa <mohammedwalaar.sw@student.uobabylon.edu.iq>
2024-11-13 20:36:22 +0300

    first

---
+++ tables.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE IF NOT EXISTS  admins(
+    id SERIAL PRIMARY KEY,
+    name  text  NOT NULL,
+    password  text  NOT NULL,
+    email  text unique  NOT NULL,
+    time TIMESTAMP DEFAULT NOW()
+);
+CREATE TABLE IF NOT EXISTS admins_tokens (
+    id SERIAL PRIMARY KEY,
+    token text NOT NULL,
+    ip text NOT NULL ,
+    user_agent text NOT Null ,
+    time TIMESTAMP DEFAULT NOW() ,
+    admin_id INTEGER NOT NULL  REFERENCES admins(id)
+);
