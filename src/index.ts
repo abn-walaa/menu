ac09ef212d4d25539e4c4ca081c8a42770540964
Mohammed Walaa <mohammedwalaar.sw@student.uobabylon.edu.iq>
2024-11-15 12:57:48 +0300

    a'

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,30 @@
 import { Hono, } from 'hono'
 import { HTTPException, } from 'hono/http-exception'
 import { showRoutes, } from 'hono/dev'
-import  langs from "@routers/langs"
-
-import Admin from '@routers/admin/log'
-import { DatabaseError } from "pg"
+import langs from "@routers/langs"
 const app = new Hono({
-  
-});
 
+
+
+});
 
 app.get('/subdomin/*', (c) => c.text("hello sub   " + c.req.path));
 
 app.get('/*', (c) => {
   console.log("-----------------------");
-  throw new HTTPException(404, {
-    message: c.req.url + " " + c.req.path, cause: {
+  throw new HTTPException(500, {
+    message: ":D", cause: {
       hello: "aa"
     }
   })
+  return c.text('Matched route with .r');
 });
 
-
-
-
-
-app.route('/admin', Admin)
-app.route('/langs',langs);
+app.route('/langs', langs);
 app.onError((err, c) => {
   // console.log(err.constructor.name)
   // console.log(err)
-  if (err instanceof DatabaseError) {
-    console.error(err.constraint)
-  }
+
   return c.json({
     status: "done",
     message: err.message,
@@ -47,9 +39,7 @@ showRoutes(app, {
 
 
 export default {
-  
+
   fetch: app.fetch,
-
   port: 3000
-
 }
